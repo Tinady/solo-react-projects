2bde290924dc37adb5971a9f1a53bd2673b7d707
Tina Dessu <tinady2009@gmail.com>
2023-04-04 16:59:38 +0300

    Navbar

--- staticwebsite/src/App.js
+++ staticwebsite/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div >
+      <Navbar/>
     </div>
   );
 }
---
+++ staticwebsite/src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import logo from  "../imgs/logo.png"
+import hamburger from "../imgs/hamburger.jpg"
+
+
+export default  function Navbar()
+{
+   return( 
+   <div className="navbar">
+        <div className="logo-container" >
+            <img className="logo" src={logo}/>
+            <p className="logo-text">Botanic Garden</p>
+        </div>
+        
+        <div className="navbar-buttons">
+        <button className="btn">Visit </button> 
+        <button className="btn">Donate</button>
+     
+        </div>
+        
+       
+
+    </div>
+   )
+
+
+}
+
+
