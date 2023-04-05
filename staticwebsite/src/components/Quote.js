69c4afbdf3a61f3f63ca8bf9ce57f6d7577806a3
Tina Dessu <tinady2009@gmail.com>
2023-04-05 16:24:49 +0300

    Update Quote.js

--- staticwebsite/src/components/Quote.js
+++ staticwebsite/src/components/Quote.js
@@ -8,7 +8,9 @@ export default function Quote()
             <img className="quote-img" src={background}/>
             <div className="garden-quote">
             <p className="quote-text"><q>A garden is not just a place.
-                 It is also a journey.</q></p>
+                 It is also a journey.</q> Start your journey with us! Subscribe for 
+                 the latest news and events.</p>
+                 <button className="subscribe-btn">  Subscribe</button>
                  </div>
                </div>
     )
