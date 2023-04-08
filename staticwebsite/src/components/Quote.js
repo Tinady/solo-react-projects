fe615e1a24a159c5c95c6f450abca650e721bd29
Tina Dessu <tinady2009@gmail.com>
2023-04-08 20:10:48 +0300

    Update Quote.js

--- staticwebsite/src/components/Quote.js
+++ staticwebsite/src/components/Quote.js
@@ -1,17 +1,17 @@
 import React from "react";
-import background from "../imgs/background1.avif"
+
 
 export default function Quote()
 {
     return(
         <div className="quote-container">
-            <img className="quote-img" src={background}/>
-            <div className="garden-quote">
+          
+          
             <p className="quote-text"><q>A garden is not just a place.
                  It is also a journey.</q> Start your journey with us! Subscribe for 
                  the latest news and events.</p>
                  <button className="subscribe-btn">  Subscribe</button>
-                 </div>
+              
                </div>
     )
 }
