94ed7dc3c7e2f9756a55f0b2ffcf9a42ff72b983
Tina Dessu <tinady2009@gmail.com>
2023-04-10 00:32:17 +0300

    Update header.js

--- travel-journal/src/components/header.js
+++ travel-journal/src/components/header.js
@@ -1,11 +1,14 @@
 import React from "react";
-import logo from "../imgs/logo.jpg"
+import logo from "../imgs/logo.png"
 
 export default function Header(){
     return(
         <div className="header">
-            <img src={logo}/>
-            <p>Travel journal</p>
+           <div className="journal">
+             <img src={logo}/>
+             <p>Travel journal</p>
+         </div> 
+           
         </div>
     )
 }
