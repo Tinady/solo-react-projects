ef3de0dec29d8f2871738752a7e945ed189ea482
Tina Dessu <tinady2009@gmail.com>
2023-04-10 17:56:21 +0300

    Update App.js

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
        <Header/>
-       <div className='cards-container'>
+       <div className='cards'>
         {cards}
        </div>
     </div>
