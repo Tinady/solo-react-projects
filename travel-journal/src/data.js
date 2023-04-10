dc3654799169fefe00c8bba4fd8e8e3fe1d9fa8c
Tina Dessu <tinady2009@gmail.com>
2023-04-10 16:52:47 +0300

    Create data.js

---
+++ travel-journal/src/data.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Mount from "../img/Mount-Fuji.png";
+
+export default [
+{
+ id:1,
+ image:{Mount},
+ country: "Japan",
+ location: "Mount-Fuji",
+ date: "12 Jan, 2021 - 24 Jan, 2021",
+ description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan,for both Japanese and foreign tourists."
+
+
+}
+]
