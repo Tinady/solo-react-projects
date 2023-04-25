fe095d0c749169be61cdbbd507c3763e918e8a48
Tina Dessu <tinady2009@gmail.com>
2023-04-25 15:24:19 +0300

    Create intro.jsx

---
+++ trial/src/components/intro.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Intro(){
+    return(
+        <div className="intro">
+          <div className="intro-description">
+            <h1>
+                
+            </h1>
+
+          </div>
+
+
+        </div>
+    )
+}
