529271933ea6b1baf0bfc1f0ef2c79fc73a3a6f0
Tina Dessu <tinady2009@gmail.com>
2023-04-25 13:23:12 +0300

    Update App.jsx

--- trial/src/App.jsx
+++ trial/src/App.jsx
@@ -3,11 +3,16 @@ import Header from './components/Header'
 import './App.css'
 
 function App() {
-  
+
+  const [darkmode, setDarkMode]=useState(true)
+    function toggle()
+    {
+       setDarkMode(prevState=>!prevState)
+    }
 
   return (
     <div className="App">
-      <Header/>
+      <Header darkMode={darkmode} handleClick={toggle}/>
     </div>
   )
 }
