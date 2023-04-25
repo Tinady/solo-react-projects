05104df4007d2006e692db75152e909d27642bba
Tina Dessu <tinady2009@gmail.com>
2023-04-25 13:23:06 +0300

    Update Header.jsx

--- trial/src/components/Header.jsx
+++ trial/src/components/Header.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import logo from "../assets/company_logo.png"
 
-export default function Header(){
+export default function Header(props){
   return(
-    <nav>
+    <nav className={props.darkMode?'dark':''}>  
       <div className="logos-container">
        <img src={logo} className="logo"/>
+       <p>Company name</p>
       </div>
 
-      <p>Services and prices</p>
+      <div className="link-container">
+
+      <a href="">Services and prices 
+      </a>
       <button>Sign Up</button>
-      <div className="toggle-Container">
-        <div  className="toggle-slider"> </div>
-        <div className="toggle-circle"></div>
+      <div className="toggle-Container" onClick={props.handleClick}>
+        <div  className="toggle-slider" >
+           <div className="toggle-circle"></div>
+         </div>
        
+       
+      </div>
       </div>
       
 
