dd33616912eea36851716a5f618796d30196e384
Tina Dessu <tinady2009@gmail.com>
2023-04-26 10:47:15 +0300

    Update intro.jsx

--- trial/src/components/intro.jsx
+++ trial/src/components/intro.jsx
@@ -4,9 +4,8 @@ export default function Intro(){
     return(
         <div className="intro">
           <div className="intro-description">
-            <h1>
-                
-            </h1>
+            <h1> Exceed you limit </h1>
+            <p> </p>
 
           </div>
 
