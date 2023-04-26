ea816d0df6aa3a3ca62bb5f012343d85bfbca4ef
Tina Dessu <tinady2009@gmail.com>
2023-04-26 10:33:01 +0300

    Update Header.jsx

--- trial/src/components/Header.jsx
+++ trial/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "../assets/company_logo.png"
 import icon from "../assets/dropdown.png"
+import hamburger from "../assets/hamburger.png"
 
 export default function Header(props){
   return(
@@ -13,16 +14,17 @@ export default function Header(props){
       <div className="link-container">
 
       <a href="">Services and prices 
-      <div><img className="dropdown"src={icon}/>
-      </div> </a>
+      <img className="dropdown"src={icon}/>
+      </a>
       <a href="">Sign Up</a>
-      <div className="toggle-Container" onClick={props.handleClick}>
+        <div className="toggle-Container" onClick={props.handleClick}>
         <div  className="toggle-slider" >
            <div className="toggle-circle"></div>
          </div>
-       
-       
-      </div>
+        </div>
+        <button className="btn">
+        <img src={hamburger}/>
+        </button>
       </div>
       
 
