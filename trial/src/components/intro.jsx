fef36017c23aede5a60b8d744dbebf955716a95e
Tina Dessu <tinady2009@gmail.com>
2023-04-27 16:15:46 +0300

    Update intro.jsx

--- trial/src/components/intro.jsx
+++ trial/src/components/intro.jsx
@@ -4,9 +4,19 @@ export default function Intro(props){
     return(
         <div className={props.darkMode?"introdark":"intro"}>
           <div className="intro-description">
-            <h1> Exceed you limit </h1>
-            <p> </p>
+            <h1> Exceed your limit </h1>
+            <p>Generating random paragraphs 
+              can be an excellent way for writers 
+              to get their creative flow going at
+               the beginning of the day. The writer
+                has no idea what topic the random 
+                paragraph will be about when it appears. 
+                This forces the writer to use creativity to
+                 complete one of three common writing challenges.
+                  The writer can use the paragraph as 
+              the first one of a short story and build</p>
 
+            <button>Start your free Trial </button>
           </div>
 
 
