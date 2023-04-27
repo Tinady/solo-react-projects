00d166da6a317ce4a983b85c66d0220151596172
Tina Dessu <tinady2009@gmail.com>
2023-04-27 09:51:58 +0300

    Update intro.jsx

--- trial/src/components/intro.jsx
+++ trial/src/components/intro.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Intro(){
+export default function Intro(props){
     return(
-        <div className="intro">
+        <div className={props.darkMode?"introdark":"intro"}>
           <div className="intro-description">
             <h1> Exceed you limit </h1>
             <p> </p>
