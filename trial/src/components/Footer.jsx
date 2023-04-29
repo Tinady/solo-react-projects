68aabe848f1b359c84cbbd10a19f572d8a25911e
Tina Dessu <tinady2009@gmail.com>
2023-04-29 12:24:28 +0300

    Update Footer.jsx

--- trial/src/components/Footer.jsx
+++ trial/src/components/Footer.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+
+export default  function Footer(props){
+ return(
+    <div className={props.darkMode?"footer-dark":"footer-container"}>
+        <p>FAQ</p>
+        <p>Contact us</p>
+        <p>random</p>
+    </div>
+ )
+}
