440573bb508d35445d826486222259cc0b01f794
Tina Dessu <tinady2009@gmail.com>
2023-05-08 22:02:20 +0300

    Update Navbar.jsx

--- DNM/src/components/Navbar.jsx
+++ DNM/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import {Link} from "react-router-dom"
 import "./navbar.css"
 import logo from '../assets/logo2.png'
+import search from '../assets/search.png'
  export default  function Navbar(){
     return(
         <nav >
         <img className="logo" src={logo} alt=""/>
         <div className="search">
            <input type="text" placeholder="search.."/>
-           <div className="search-img">
-            
+           <div className="search-img-container">
+             <img className="search-img" src={search} alt=""/>
            </div>
         </div>
         <div className="links-container">
-           
+            <Link to='/' className='links'>Home</Link>
+            <Link to='/' className='links'>Home</Link>
+            <Link to='/' className='links'>Home</Link>
+            <Link to='/' className='links'>Home</Link>
         </div>
         </nav>
     )
