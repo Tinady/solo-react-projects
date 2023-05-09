1a937e297d190e0840aef96edd54ceba052ffc0c
Tina Dessu <tinady2009@gmail.com>
2023-05-09 19:40:24 +0300

    Update App.jsx

--- DNM/src/App.jsx
+++ DNM/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
+
+import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-
+ 
   return (
     <>
      <Navbar/>
      <Routes>
       <Route path='/home' element={<Home/>}/>
-
+      <Route path='/articles' element={<>Articles</>}/>
+      <Route path='/login' element={<>Login</>}/>
       
      </Routes>
     </>
