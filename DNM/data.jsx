c1ada5d61eb93ece3ff1da0999835e3106a2da64
Tina Dessu <tinady2009@gmail.com>
2023-05-10 17:46:11 +0300

    Create data.jsx

---
+++ DNM/data.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default[
+      {
+        id:1,
+        name:'Abebe Terefe',
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+]
