ee3da77c66a34387f83830ddd4ce9139f620b48a
Tina Dessu <tinady2009@gmail.com>
2023-05-11 14:56:04 +0300

    Create data.jsx

---
+++ DNM/src/data.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import image from './assets/doc1.jpg'
+
+export default[
+      {
+        id:1,
+        name:'Abebe Terefe',
+        img:image,
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        img:image,
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        img:image,
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye doctor'
+        
+
+      },
+
+
+      {
+        id:2,
+        name:'Abebe Terefe',
+        img:image,
+        Hospital: 'return',
+        when:'3:34-3:60',
+        specialization:'Eye'
+        
+
+      },
+
+]
