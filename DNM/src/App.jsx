d767d19621e046a47118824091cbc8aeca96b9f1
Tina Dessu <tinady2009@gmail.com>
2023-05-11 14:56:19 +0300

    Update App.jsx

--- DNM/src/App.jsx
+++ DNM/src/App.jsx
@@ -2,15 +2,20 @@
 import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home'
 import { Route, Routes } from 'react-router-dom'
+import data from './data'
 import './App.css'
 
 function App() {
- 
+
+  const homeel= data.map(items=>
+     {return <Home key={items.id} items={items}/>})
+   
+     
   return (
     <>
      <Navbar/>
      <Routes>
-      <Route path='/home' element={<Home/>}/>
+      <Route path='/' element={<div className='cards-container'>{homeel}</div>}/>
       <Route path='/articles' element={<>Articles</>}/>
       <Route path='/login' element={<>Login</>}/>
       
