c0785cf58f6e3ff0293ee7a649b4d468051d5097
Tina Dessu <tinady2009@gmail.com>
2023-05-12 21:18:20 +0300

    Update Home.jsx

--- DNM/src/components/home/Home.jsx
+++ DNM/src/components/home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home(props){
     return(
         <div className="card-container">
          <img src={props.items.img} alt=""/>
-        <h1>Dr. {props.items.name}</h1>
+        <p>Dr. {props.items.name}</p>
         <p>Works in: {props.items.Hospital}</p>
         <p>hours:{props.items.when}</p>
         <p>{props.items.specialization}</p>
