635eb8df38cc7516e24f4fb35f7ca0ccbb4ce707
Tina Dessu <tinady2009@gmail.com>
2023-05-13 23:04:39 +0300

    Update data.jsx

--- DNM/src/data.jsx
+++ DNM/src/data.jsx
@@ -8,7 +8,8 @@ export default[
         img:image,
         Hospital: 'return',
         when:'3:34-3:60',
-        specialization:'Eye'
+        specialization:'Eye',
+        fav:false
         
 
       },
@@ -19,40 +20,44 @@ export default[
         img:image,
         Hospital: 'return',
         when:'3:34-3:60',
-        specialization:'Eye'
+        specialization:'Eye',
+        fav:false
         
 
       },
 
       {
-        id:2,
+        id:3,
         name:'Solomon Feyisa',
         img:image,
         Hospital: 'return',
         when:'3:34-3:60',
-        specialization:'Eye doctor'
+        specialization:'Eye doctor',
+        fav:false
         
 
       },
 
 
       {
-        id:2,
+        id:4,
         name:'Abel Kebede',
         img:image,
         Hospital: 'return',
         when:'3:34-3:60',
-        specialization:'Eye'
+        specialization:'Eye',
+        fav:false
         
 
       },
       {
-        id:2,
+        id:5,
         name:'Mekonin Terefe',
         img:image,
         Hospital: 'return',
         when:'3:34-3:60',
-        specialization:'Eye'
+        specialization:'Eye',
+        fav:false
         
 
       }
