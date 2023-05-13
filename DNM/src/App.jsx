4b07139fcd3e971653f0e3d515840ad32b9b6a23
Tina Dessu <tinady2009@gmail.com>
2023-05-13 17:16:43 +0300

    Update App.jsx

--- DNM/src/App.jsx
+++ DNM/src/App.jsx
@@ -1,6 +1,8 @@
 
 import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home'
+import Login from './components/login/login'
+import Signup from './components/signup/signup'
 import { Route, Routes } from 'react-router-dom'
 import data from './data'
 import './App.css'
@@ -17,7 +19,8 @@ function App() {
      <Routes>
       <Route path='/' element={<div className='cards-container'>{homeel}</div>}/>
       <Route path='/articles' element={<>Articles</>}/>
-      <Route path='/login' element={<>Login</>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='signup'  element={<Signup/>}/>
       
      </Routes>
     </>
