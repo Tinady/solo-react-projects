0c72e6c1047589b06f5086f504cbec6253f243ec
Tina Dessu <tinady2009@gmail.com>
2023-05-13 23:05:05 +0300

    Update Home.jsx

--- DNM/src/components/home/Home.jsx
+++ DNM/src/components/home/Home.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import "./home.css";
+import { Link } from "react-router-dom";
 
 export default function Home(props){
     return(
         <div className="card-container">
           <div>
-         <img src={props.items.img} alt=""/>
-        <p>Dr. {props.items.name}</p>
+         <button className="fav-btn">
+          fav
+         </button>
+      <img src={props.items.img} alt=""/>
+       <p>Dr. {props.items.name}</p>
         <p>Works in: {props.items.Hospital}</p>
         <p>hours:{props.items.when}</p>
         <p>{props.items.specialization}</p>
+        <Link to='/:{props.items.id}'>
         <button>Read more</button>
+        </Link>
         </div>
       </div>
     )
