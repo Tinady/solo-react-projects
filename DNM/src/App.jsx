1d5218b35eca236aa6271833f39ace5bdc4e97f5
Tina Dessu <tinady2009@gmail.com>
2023-05-14 23:12:23 +0300

    Update App.jsx

--- DNM/src/App.jsx
+++ DNM/src/App.jsx
@@ -1,23 +1,47 @@
-
 import Navbar from './components/navbar/Navbar'
+import Fav from './components/fav/fav'
 import Home from './components/home/Home'
+import ReadMore from './components/readmore/readmore'
 import Login from './components/login/login'
 import Signup from './components/signup/signup'
 import { Route, Routes } from 'react-router-dom'
 import data from './data'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+    const [docs, setDocs]=useState(data)
 
-  const homeel= data.map(items=>
-     {return <Home key={items.id} items={items}/>})
+    function toggle(id){
+            setDocs(prevState=>{
+              return prevState.map((docs)=>{
+                return docs.id===id?{...docs, fav:!docs.fav}:docs;
+              })
+            })
+    }
+
+  const homeel= docs.map(items=>
+     {return <Home key={items.id}
+      items={items} 
+      handleclick={()=>toggle(items.id)}/>})
+
+  const favel= docs.map(items=>
+        {return <Fav key={items.id}
+         items={items} 
+         handleclick={()=>toggle(items.id)}/>})
    
-     
+     const rm=<ReadMore items={docs}/>
   return (
     <>
      <Navbar/>
      <Routes>
-      <Route path='/' element={<div className='cards-container'>{homeel}</div>}/>
+      <Route path='/' element={<div>
+        <div className='cards-container'>{homeel}</div>
+        <h2>Favourites</h2>
+        <hr/>
+        <div className='cards-container'>{favel}</div>
+        </div>}/> 
+      <Route path='/:id' element={rm}/>
       <Route path='/articles' element={<>Articles</>}/>
       <Route path='/login' element={<Login/>} />
       <Route path='signup'  element={<Signup/>}/>
