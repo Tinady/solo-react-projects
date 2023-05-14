029111878af5ea5e7e1536b8aaae6548dc5ca2df
Tina Dessu <tinady2009@gmail.com>
2023-05-14 23:12:12 +0300

    Create fav.jsx

---
+++ DNM/src/components/fav/fav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Fav(props){
+  return (
+    <div>
+      {props.items.fav &&  <div className="card-container">
+          <div>
+         <button className="fav-btn"  
+          onClick={props.handleclick}>
+          unfav
+         </button>
+      <img src={props.items.img} alt=""/>
+       <p>Dr. {props.items.name}</p>
+        <p>Works in: {props.items.Hospital}</p>
+        <p>hours:{props.items.when}</p>
+        <p>{props.items.specialization}</p>
+        <Link to={`./${props.items.id}`}>
+        <button>Read more</button>
+        </Link>
+        </div>
+        </div>}
+    </div>
+  )
+}
