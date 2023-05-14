49c1e3400433a27c90dcb11ebba5ed0d5f20e64c
Tina Dessu <tinady2009@gmail.com>
2023-05-14 23:11:52 +0300

    Create readmore.jsx

---
+++ DNM/src/components/readmore/readmore.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './readmore.css'
+import { useParams } from "react-router-dom";
+
+export default function ReadMore(props){
+    const {id}=useParams();
+    const doc=props.items[id]
+   
+  return(
+    <diV className='details'>
+    <div className="details-container">
+    <img src={doc.img}/> 
+    <div>
+    <h1>{doc.name}</h1> 
+    <p>{doc.when}</p>
+    <p>{doc.hospital}</p>  
+    </div>
+    </div>
+    
+    </diV>
+    
+    )
+}
