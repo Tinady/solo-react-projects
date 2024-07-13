23d5f86f80cc58ab8b9a6fa9ffa654502a169e62
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-13 21:45:36 +0300

    Update Hero.jsx

--- Nike/src/components/Hero.jsx
+++ Nike/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-   <section id='home' className='w-full border-2 max-container  border-red-500'x>
+   <section id='home' className='w-full border-2 max-container flex flex-col  justify-center min-h-screen  xl:flex-row  border-red-500'x>
 
    </section>
   )
