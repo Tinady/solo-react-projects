f0a86da84a06786e99e8640f377a1606854fecfa
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-15 15:13:43 +0300

    Update App.jsx

--- gsap/src/App.jsx
+++ gsap/src/App.jsx
@@ -1,13 +1,20 @@
-import 'App.css'
 
-const App = () =>{
-  return(
+
+import './App.css'
+
+function App() {
+
+
+  return (
     <>
+      <div className='parent'>
+        Parent
+        <div className='child-one  child'>One</div>
+        <div className='child-two  child'>Two</div>
+        <div className='child-three  child'>Three</div>
+      </div>
     </>
   )
-
 }
 
 export default App
-
-
