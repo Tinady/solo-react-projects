34b5d1eaf4ae37bbde3e6784946a2be9efc6609f
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-17 19:23:21 +0300

    Update App.jsx

--- gsap/src/App.jsx
+++ gsap/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-<form>
-  <input type='text'/>
-
-</form>
+<div>
+<NavBar/>
+</div>
   )
 }
 
