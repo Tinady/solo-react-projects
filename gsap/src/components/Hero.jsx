8b29090d207b38db7104e9b28a4a3fe42eff236b
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-21 23:11:51 +0300

    Update Hero.jsx

--- gsap/src/components/Hero.jsx
+++ gsap/src/components/Hero.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import { Hero1 } from "../assets"
 
 const Hero = () => {
   return (
-    <section  className='bg-[url("../assets/hero.jpg")]' >
-    <div>
-      <input className=''/>
-    </div>
+    <section className='bg-cover mt-20 h-[89vh] flex items-center justify-center flex-col gap-10' style={{ backgroundImage: `url(${Hero1})` }}>
+      
+        <input className='w-10/12 h-12 rounded-xl px-8 focus:ring-0' type='text' placeholder='Search here ....' />
+        <h2 className='text-xl md:text-3xl text-[#5B5F97] font-bold' >We thrive to make staying healthy easier</h2>
+        
+        
+   
     </section>
   )
 }
