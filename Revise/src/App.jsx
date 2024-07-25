db8998ed7738f10b8b429f1513760aac225a54e9
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-07-25 17:31:05 +0300

    update

--- Revise/src/App.jsx
+++ Revise/src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import  './App.css'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <form className='new-item-form'>
-     <div className='form-row'>
-      <label htmlFor='item' >
-        New Item
-      </label>
-      <input type='text' id='item'/>
-     </div>
-    </form>
-  )
-}
+    <>
+      {" "}
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1>Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+};
 
-export default App
+export default App;
